300c3728093ddbb87af8e915d5fd9541130decc1
Ryan Ragasa <RJ@ryans-imac.home>
2018-08-30 10:59:59 -0700

    Changed the name of file "PriorityQueue.py" to "PriorityQueueClass.py"

---
+++ Assignment_One/PriorityQueueClass.py
@@ -0,0 +1,17 @@
+class PriorityQueue:
+
+    def __init__(self):
+        self.pQueue = []
+
+    def addNewElement(self):
+        print("Adding new element")
+
+    def returnHighestPriorityElement(self):
+        print("Here is the highest element")
+
+    def removeHighestElement(self):
+        print("Removing the highest element")
+
+
+myPriorityQueue = PriorityQueue()
+
