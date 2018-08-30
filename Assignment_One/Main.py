82fa758607fbf773ac39a94af3d6f617f8a3deaf
Ryan Ragasa <RJ@ryans-imac.home>
2018-08-30 15:03:21 -0700

    Got the PriorityQueueClass.py to add an element to the left of the queue if it's greater than the center and less than or equal to the one neighboring it.

--- Assignment_One/Main.py
+++ Assignment_One/Main.py
@@ -2,14 +2,30 @@ from Assignment_One import StudentClass
 from Assignment_One import PriorityQueueClass
 
 studentNathan = StudentClass.Student("Nathan", "777777777", "Nate@aol.com", "134 Home St", 4.0, 150)
+studentDavid = StudentClass.Student("David", "111111111", "David@aol.com", "134 Yolo St", 2.0, 75)
+studentEric = StudentClass.Student("Eric", "222222222", "Eric@aol.com", "134 Sky St", 3.4, 20)
+
 priorityQueue = PriorityQueueClass.PriorityQueue()
 
-print(studentNathan.name)
-print(studentNathan.redId)
-print(studentNathan.email)
-print(studentNathan.address)
-print(studentNathan.gpa)
-print(studentNathan.numberOfUnitsTaken)
-print(studentNathan.studentPriority)
+print("{}'s priority is {}".format(studentNathan, studentNathan.studentPriority))
+print("{}'s priority is {}".format(studentDavid, studentDavid.studentPriority))
+print("{}'s priority is {}".format(studentEric, studentEric.studentPriority))
 
 print(priorityQueue.pQueue)
+
+priorityQueue.addNewElement(studentNathan)
+priorityQueue.pQueue.append(studentDavid)
+priorityQueue.pQueue.append(studentEric)
+
+for student in priorityQueue.pQueue:
+    print(student.studentPriority)
+
+print(priorityQueue.pQueue)
+
+studentDavi = StudentClass.Student("David", "111111111", "David@aol.com", "134 Yolo St", 2.5, 75)
+priorityQueue.addNewElement(studentDavi)
+
+print(priorityQueue.pQueue)
+
+for student in priorityQueue.pQueue:
+    print(student.studentPriority)
--- Assignment_One/PriorityQueueClass.py
+++ Assignment_One/PriorityQueueClass.py
@@ -1,10 +1,34 @@
+from Assignment_One import StudentClass
+
+
 class PriorityQueue:
 
     def __init__(self):
         self.pQueue = []
 
-    def addNewElement(self):
-        print("Adding new element")
+    def addNewElement(self, studentObject = StudentClass.Student):
+        #If the pQueue is empty, append to the pQueue
+        if len(self.pQueue) == 0:
+            self.pQueue.append(studentObject)
+        else:
+            #Continuously divide the length of the pQueue by 2 and check to see if the student priority
+            #is more or less based on the neighboring student object
+            priorityQueueDivider = int(len(self.pQueue)/2) # priorityQueueDivider = 0
+
+            while priorityQueueDivider != -1:
+                #Check if the student priority is greater than the current divider position...
+                #If so the student will be placed on the left side of the pQueue, else the right side.
+                if studentObject.studentPriority >= self.pQueue[priorityQueueDivider].studentPriority:
+                    #If the student will be placed on the left side, check the element directly to the left of it...
+                    #If the element is less than or equal to that element, the element will be placed there.
+                    if studentObject.studentPriority <= self.pQueue[priorityQueueDivider - 1].studentPriority:
+                        leftSideOfQueue = self.pQueue[:priorityQueueDivider]
+                        leftSideOfQueue.append(studentObject)
+                        rightSideOfQueue = self.pQueue[priorityQueueDivider:]
+                        self.pQueue = leftSideOfQueue + rightSideOfQueue
+
+                        #stop the program
+                        priorityQueueDivider = -1
 
     def returnHighestPriorityElement(self):
         print("Here is the highest element")
