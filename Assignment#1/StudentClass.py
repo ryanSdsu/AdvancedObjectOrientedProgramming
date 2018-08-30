6594b11c0337c76e78d5cc623520d26c6fe1faca
Ryan Ragasa <RJ@ryans-imac.home>
2018-08-30 10:31:00 -0700

    Updated the StudentClass to have try and except catches for the gpa, numberOfUnitsTaken and studentPriority

--- Assignment#1/StudentClass.py
+++ Assignment#1/StudentClass.py
@@ -5,7 +5,7 @@
 
 class Student:
     """This is the base class for all students"""
-    def __init__(self, name, redId, email, address, gpa, numberOfUnitsTaken):
+    def __init__(self, name, redId, email, address, gpa=None, numberOfUnitsTaken=None):
         """
         Constructor
         :param str name: The name of the student
@@ -19,17 +19,40 @@ class Student:
         self.redId = str(redId)
         self.email = str(email)
         self.address = str(address)
-        if gpa >= 0 and gpa <= 4.0:
-            self.gpa = float(gpa) #The gpa must be between 0 and 4.0
-        if numberOfUnitsTaken >= 0 and numberOfUnitsTaken <= 150:
-            self.numberOfUnitsTaken = int(numberOfUnitsTaken) #The number of units taken must be between 0 and 150
-        self.studentPriority = float(0.7*self.numberOfUnitsTaken + 0.3*self.gpa)
 
-studentOne = Student("Nate", "777777777", "Nate@aol.com", "134 Home St", 4.0, 150)
-print(studentOne.name)
-print(studentOne.redId)
-print(studentOne.email)
-print(studentOne.address)
-print(studentOne.gpa)
-print(studentOne.numberOfUnitsTaken)
-print(studentOne.studentPriority)
+        try:
+            #Attempt to create gpa for the student
+            if gpa >= 0 and gpa <= 4.0: #The gpa must be between 0 and 4.0
+                self.gpa = float(gpa)
+            self.gpa
+        except AttributeError:
+            print("The gpa of {} is not between 0 and 4.0.".format(self.name))
+        except TypeError:
+            print("The gpa is not an actual number.".format(self.name))
+
+        try:
+            #Attempt to create numberOfUnits taken for the student
+            if numberOfUnitsTaken >= 0 and numberOfUnitsTaken <= 150: #The number of units taken must be between 0 and 150
+                self.numberOfUnitsTaken = int(numberOfUnitsTaken)
+            self.numberOfUnitsTaken
+        except AttributeError:
+            print("The number of units taken from {} is not between 0 and 150.".format(self.name))
+        except TypeError:
+            print("The number of units taken is not an actual number.".format(self.name))
+
+        try:
+            #Attempt to create studentPriority for the student
+            if self.gpa and self.numberOfUnitsTaken: #Both the gpa and number of units taken must be valid
+                self.studentPriority = float(0.7*self.numberOfUnitsTaken + 0.3*self.gpa)
+        except AttributeError:
+            print("Both the gpa and number of units taken for {} are not valid.".format(self.name))
+
+
+studentOne = Student("Nathan", "777777777", "Nate@aol.com", "134 Home St", 4.0, 150)
+# print(studentOne.name)
+# print(studentOne.redId)
+# print(studentOne.email)
+# print(studentOne.address)
+# print(studentOne.gpa)
+# print(studentOne.numberOfUnitsTaken)
+# print(studentOne.studentPriority)
