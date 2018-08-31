f389731eafa9d818a3349aa12becbefcb2f0b4b8
Ryan Ragasa <RJ@ryans-imac.home>
2018-08-31 12:37:12 -0700

    For the "PriorityQueueClass.py", I finished the addNewObject definition. There are unit tests below which need to be moved but prove that it works. 100% verified.

--- Assignment_One/PriorityQueueClass.py
+++ Assignment_One/PriorityQueueClass.py
@@ -2,33 +2,46 @@ from Assignment_One import StudentClass
 
 
 class PriorityQueue:
+    """This is the base class for the Priority Queue, the main structure is a max heap"""
 
     def __init__(self):
-        self.pQueue = []
+        """
+        This is the constructor for the PriorityQueue class.
+        The initial 'None' ensures that students being added to the list start on index 1
+        """
+        self.priorityQueue = [None]
 
-    def addNewElement(self, studentObject = StudentClass.Student):
-        #If the pQueue is empty, append to the pQueue
-        if len(self.pQueue) == 0:
-            self.pQueue.append(studentObject)
-        else:
-            #Continuously divide the length of the pQueue by 2 and check to see if the student priority
-            #is more or less based on the neighboring student object
-            priorityQueueDivider = int(len(self.pQueue)/2) # priorityQueueDivider = 0
+    def addNewObject(self, studentObject=StudentClass.Student):
+        """
+        This is where we add a new object into the priorityQueue.  The main structure is a max heap therefore
+        all of the objects being added must traverse to the top of the list based on the highest priority. As a
+        reminder the parents of each newly added object in a max heap is the "floor" of the index / 2.
+        :param StudentClass.Student studentObject:
+        :return: priorityQueue
+        :rtype: []
+        """
 
-            while priorityQueueDivider != -1:
-                #Check if the student priority is greater than the current divider position...
-                #If so the student will be placed on the left side of the pQueue, else the right side.
-                if studentObject.studentPriority >= self.pQueue[priorityQueueDivider].studentPriority:
-                    #If the student will be placed on the left side, check the element directly to the left of it...
-                    #If the element is less than or equal to that element, the element will be placed there.
-                    if studentObject.studentPriority <= self.pQueue[priorityQueueDivider - 1].studentPriority:
-                        leftSideOfQueue = self.pQueue[:priorityQueueDivider]
-                        leftSideOfQueue.append(studentObject)
-                        rightSideOfQueue = self.pQueue[priorityQueueDivider:]
-                        self.pQueue = leftSideOfQueue + rightSideOfQueue
+        # To begin we will confirm if the newly added object has any "parents".
+        # Since the parents location of the newly added object is the "floor" of the newly added objects index / 2,
+        # we can simply compute this by shifting the bits of the newly added objects index to the right.
+        self.priorityQueue.append(studentObject)
+        newObjectLocation = len(self.priorityQueue) - 1
+        newObjectParentLocation = len(self.priorityQueue) - 1
+        newObjectParentLocation = newObjectParentLocation >> 1
 
-                        #stop the program
-                        priorityQueueDivider = -1
+        # If the newly added object has parents we can now check the 'studentPriority'.
+        # If the newly added objects 'studentPriority' is lower than that of it's parents, we will traverse it upwards
+        # on the priority queue by swapping their locations.
+        while (newObjectParentLocation > 0):
+            if studentObject.studentPriority > self.priorityQueue[newObjectParentLocation].studentPriority:
+                swapTemp = self.priorityQueue[newObjectParentLocation]
+                self.priorityQueue[newObjectParentLocation] = studentObject
+                self.priorityQueue[newObjectLocation] = swapTemp
+                newObjectLocation = newObjectParentLocation
+                newObjectParentLocation = newObjectParentLocation >> 1
+            else:
+                newObjectParentLocation = newObjectParentLocation >> 1
+        return self.priorityQueue
 
     def returnHighestPriorityElement(self):
         print("Here is the highest element")
@@ -37,5 +50,40 @@ class PriorityQueue:
         print("Removing the highest element")
 
 
+#Unit Test for addNewObject
+studentDavid = StudentClass.Student("David", "111111111", "David@aol.com", "134 Yolo St", 2.0, 75)
+studentEric = StudentClass.Student("Eric", "777777777", "Nate@aol.com", "134 Home St", 3.0, 100)
+studentNathan = StudentClass.Student("Nathan", "777777777", "Nate@aol.com", "134 Home St", 3.5, 120)
+studentPaul = StudentClass.Student("Paul", "777777777", "Nate@aol.com", "134 Home St", 3.8, 148)
+studentChris = StudentClass.Student("Chris", "777777777", "Nate@aol.com", "134 Home St", 3.6, 121)
+studentBob = StudentClass.Student("Bob", "777777777", "Nate@aol.com", "134 Home St", 2.5, 7.6)
+studentLarissa = StudentClass.Student("Larissa", "777777777", "Nate@aol.com", "134 Home St", 3.25, 110)
+studentMark = StudentClass.Student("Mark", "777777777", "Nate@aol.com", "134 Home St", 3.65, 122)
+studentHeath = StudentClass.Student("Heath", "777777777", "Nate@aol.com", "134 Home St", 0, 0)
+studentSarah = StudentClass.Student("Sarah", "777777777", "Nate@aol.com", "134 Home St", 3.7, 123)
+studentAshley = StudentClass.Student("Ashley", "777777777", "Nate@aol.com", "134 Home St", 3.9, 149)
+studentRyan = StudentClass.Student("Ryan", "777777777", "Nate@aol.com", "134 Home St", 4, 150)
+studentJake = StudentClass.Student("Jake", "777777777", "Nate@aol.com", "134 Home St", 1, 1)
+studentDrake = StudentClass.Student("Drake", "777777777", "Nate@aol.com", "134 Home St", 1, 1)
+studentBlake = StudentClass.Student("Blake", "777777777", "Nate@aol.com", "134 Home St", 1, 1)
+
 myPriorityQueue = PriorityQueue()
+myPriorityQueue.addNewObject(studentDavid)
+myPriorityQueue.addNewObject(studentEric)
+myPriorityQueue.addNewObject(studentNathan)
+myPriorityQueue.addNewObject(studentPaul)
+myPriorityQueue.addNewObject(studentChris)
+myPriorityQueue.addNewObject(studentBob)
+myPriorityQueue.addNewObject(studentLarissa)
+myPriorityQueue.addNewObject(studentMark)
+myPriorityQueue.addNewObject(studentHeath)
+myPriorityQueue.addNewObject(studentSarah)
+myPriorityQueue.addNewObject(studentAshley)
+myPriorityQueue.addNewObject(studentRyan)
+myPriorityQueue.addNewObject(studentJake)
+myPriorityQueue.addNewObject(studentDrake)
+myPriorityQueue.addNewObject(studentBlake)
+
+for idx in range(1, 16):
+    print(myPriorityQueue.priorityQueue[idx].name)
 
--- Assignment_One/StudentClass.py
+++ Assignment_One/StudentClass.py
@@ -1,13 +1,17 @@
+#Questions for Whitney
+#Ask about graduation and forms being filled out
 #Class definition reference
 #https://stackoverflow.com/questions/9195455/how-to-document-a-method-with-parameters
 #Variable names reference
 #https://www.python.org/dev/peps/pep-0008/#class-names
 
+
 class Student:
-    """This is the base class for all students"""
+    """This is the base class for all students."""
+
     def __init__(self, name, redId, email, address, gpa=None, numberOfUnitsTaken=None):
         """
-        Constructor
+        This is the constructor for the Student class.
         :param str name: The name of the student
         :param str redId: The redID of the student
         :param str email: The email of the student
@@ -20,9 +24,11 @@ class Student:
         self.email = str(email)
         self.address = str(address)
 
+
+        # In this try block we will attempt to create the gpa for the student.
+        # The gpa must be between 0 and 4.0.
         try:
-            #Attempt to create gpa for the student
-            if gpa >= 0 and gpa <= 4.0: #The gpa must be between 0 and 4.0
+            if gpa >= 0 and gpa <= 4.0:
                 self.gpa = float(gpa)
             self.gpa
         except AttributeError:
@@ -30,9 +36,10 @@ class Student:
         except TypeError:
             print("The gpa is not an actual number.".format(self.name))
 
+        # In this try block we will attempt to create the numberOfUnits taken for the student.
+        # The number of units taken must be between 0 and 150.
         try:
-            #Attempt to create numberOfUnits taken for the student
-            if numberOfUnitsTaken >= 0 and numberOfUnitsTaken <= 150: #The number of units taken must be between 0 and 150
+            if numberOfUnitsTaken >= 0 and numberOfUnitsTaken <= 150:
                 self.numberOfUnitsTaken = int(numberOfUnitsTaken)
             self.numberOfUnitsTaken
         except AttributeError:
@@ -40,12 +47,12 @@ class Student:
         except TypeError:
             print("The number of units taken is not an actual number.".format(self.name))
 
+        # In this try block we will attempt to create the studentPriority for the student.
+        # In order to do this both the gpa and numberOfUnits variables must have been instantiated
         try:
-            #Attempt to create studentPriority for the student
-            if self.gpa and self.numberOfUnitsTaken: #Both the gpa and number of units taken must be valid
+            if self.gpa >= 0 and self.numberOfUnitsTaken >= 0:
                 self.studentPriority = float(0.7*self.numberOfUnitsTaken + 0.3*self.gpa)
         except AttributeError:
             print("Both the gpa and number of units taken for {} are not valid.".format(self.name))
-
 
 studentOne = Student("Nathan", "777777777", "Nate@aol.com", "134 Home St", 4.0, 150)
