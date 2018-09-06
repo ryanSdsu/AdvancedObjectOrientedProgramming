2fe2d20cf96cca15155a332e99718c450ad6fde6
Ryan Ragasa <RJ@ryans-imac.home>
2018-09-06 02:24:51 -0700

    I created @property tags and more unit tests for the "StudentClass.py". The unit tests are located in the ""

--- Assignment_One/Assignment_One_Unit_Tests/Test_StudentClass.py
+++ Assignment_One/Assignment_One_Unit_Tests/Test_StudentClass.py
@@ -2,6 +2,62 @@ import unittest
 from Assignment_One import StudentClass
 
 class TestStudentClass(unittest.TestCase):
+
+    def test_ChangeAddress(self):
+        """
+        This unit test is testing the changing of the 'address' variable. It passes when a student's 'address' variable
+        is changed by means of its setter and cross-referenced to the new desried 'address'.
+        :return:
+        """
+        newDesiredAddress = "132 New St."
+        studentOne = StudentClass.Student("Nathan", "777777777", "Nate@aol.com", "134 Home St", 4.0, 150)
+        studentOne.address = newDesiredAddress
+        self.assertEquals(studentOne.address, newDesiredAddress)
+
+    def test_ChangeEmail(self):
+        """
+        This unit test is testing the changing of the 'email' variable. It passes when a student's 'email' variable
+        is changed by means of its setter and cross-referenced to the new desried 'email'.
+        :return:
+        """
+        newEmail = "Nathaniel@aol.com"
+        studentOne = StudentClass.Student("Nathan", 777777777, "Nate@aol.com", "134 Home St", 4.0, 150)
+        studentOne.email = newEmail
+        self.assertEquals(studentOne.email, newEmail)
+
+    def test_ChangeGpa(self):
+        """
+        This unit test is testing the changing of the 'gpa' variable. It passes when a student's 'gpa' variable
+        is changed by means of its setter and cross-referenced to the new desried 'gpa'.
+        :return:
+        """
+        newGpa = 3.5
+        studentOne = StudentClass.Student("Nathan", "777777777", "Nate@aol.com", "134 Home St", 4.0, 150)
+        studentOne.gpa = newGpa
+        self.assertEquals(studentOne.gpa, newGpa)
+
+    def test_ChangeName(self):
+        """
+        This unit test is testing the changing of the 'name' variable. It passes when a student's 'name' variable
+        is changed by means of its setter and cross-referenced to the new desried 'name'.
+        :return:
+        """
+        newDesiredName = "David"
+        studentOne = StudentClass.Student("Nathan", "777777777", "Nate@aol.com", "134 Home St", 4.0, 150)
+        studentOne.name = newDesiredName
+        self.assertEquals(studentOne.name, newDesiredName)
+
+    def test_ChangeRedId(self):
+        """
+        This unit test is testing the changing of the 'redId' variable. It passes when a student's 'redId' variable
+        is changed by means of its setter and cross-referenced to the new desried 'redId'.
+        :return:
+        """
+        newRedId = 333333333
+        studentOne = StudentClass.Student("Nathan", 777777777, "Nate@aol.com", "134 Home St", 4.0, 150)
+        studentOne.redId = newRedId
+        self.assertEquals(studentOne.redId, newRedId)
+
     def test_Constructor(self):
         """
         This unit test is testing the instantiation of the 'Student' object based on the 'StudentClass' via the
@@ -9,26 +65,8 @@ class TestStudentClass(unittest.TestCase):
         'gpa' is a number between 0 - 4.0 and 'numberOfUnitsTaken' is a number between 0 - 150.
         :return:
         """
-        studentOne = StudentClass.Student("Nathan", "777777777", "Nate@aol.com", "134 Home St", 4.0, 150)
+        studentOne = StudentClass.Student("Nathan", 777777777, "Nate@aol.com", "134 Home St", 4.0, 150)
         self.assertIsInstance(studentOne, StudentClass.Student)
-
-    def test_WrongTypeGpa(self):
-        """
-        This unit test is testing the 'TypeError' check of the 'gpa' variable. It passes when the 'gpa' is set to something
-        other than an int or a float which triggers a 'TypeError'.
-        :return:
-        """
-        with self.assertRaises(TypeError):
-            StudentClass.Student("Nathan", "777777777", "Nate@aol.com", "134 Home St", "4.5", 150)
-
-    def test_UpperboundGpa(self):
-        """
-        This unit test is testing the upper bound of the 'gpa' variable. It passes when the 'gpa' is set to a number
-        greater than 4.0, thus raising a 'AttributeError'.
-        :return:
-        """
-        with self.assertRaises(AttributeError):
-            StudentClass.Student("Nathan", "777777777", "Nate@aol.com", "134 Home St", 4.1, 150)
 
     def test_LowerboundGpa(self):
         """
@@ -39,14 +77,83 @@ class TestStudentClass(unittest.TestCase):
         with self.assertRaises(AttributeError):
             StudentClass.Student("Nathan", "777777777", "Nate@aol.com", "134 Home St", -0.1, 150)
 
-    def test_WrongTypeNumberOfUnitsTaken(self):
+    def test_LowerBoundNumberOfUnitsTaken(self):
         """
-        This unit test is testing the 'TypeError' check of the 'numberOfUnitsTaken' variable. It passes when the
-        'numberOfUnitsTaken' is set to something other than an int or a float which triggers a 'TypeError'.
+        This unit test is testing the lower bound of the 'numberOfUnitsTaken' variable. It passes when the
+        'numberOfUnitsTaken' is set to a number lower than 0, thus raising a 'AttributeError'.
         :return:
         """
-        with self.assertRaises(TypeError):
-            StudentClass.Student("Nathan", "777777777", "Nate@aol.com", "134 Home St", 4.0, "150")
+        with self.assertRaises(AttributeError):
+            StudentClass.Student("Nathan", "777777777", "Nate@aol.com", "134 Home St", 4.0, -0.1)
+
+    def test_NumbersInName(self):
+        """
+        This unit test is testing the 'TypeError' check of the 'name' variable. It passes when a character other than the
+        letters A through Z are passed into 'name'.
+        :return:
+        """
+        with self.assertRaises(AttributeError):
+            StudentClass.Student("123NotAValidName", "777777777", "Nate@aol.com", "134 Home St", "4.5", 150)
+
+    def test_ReturnAddress(self):
+        """
+        This unit test is testing the return of the 'address' variable. It passes when a student's 'address' variable
+        is returned by means of its property tag and cross-referenced to the original 'address'.
+        :return:
+        """
+        originalAddress = "132 Original St."
+        studentOne = StudentClass.Student("Nathan", "777777777", "Nate@aol.com", originalAddress, 4.0, 150)
+        self.assertEquals(studentOne.address, originalAddress)
+
+    def test_ReturnEmail(self):
+        """
+        This unit test is testing the return of the 'email' variable. It passes when a student's 'email' variable
+        is returned by means of its property tag and cross-referenced to the original 'email'.
+        :return:
+        """
+        originalEmail = "Nate@aol.com"
+        studentOne = StudentClass.Student("Nathan", 777777777, originalEmail, "134 Home St", 4.0, 150)
+        self.assertEquals(studentOne.email, originalEmail)
+
+    def test_ReturnGpa(self):
+        """
+        This unit test is testing the return of the 'gpa' variable. It passes when a student's 'gpa' variable
+        is returned by means of its property tag and cross-referenced to the original 'gpa'.
+        :return:
+        """
+        originalGpa = 4.0
+        studentOne = StudentClass.Student("Nathan", "777777777", "Nate@aol.com", "134 Home St", originalGpa, 150)
+        self.assertEquals(studentOne.gpa, originalGpa)
+
+    def test_ReturnName(self):
+        """
+        This unit test is testing the return of the 'name' variable. It passes when a student's 'name' variable
+        is returned by means of its property tag and cross-referenced to the original 'name'.
+        :return:
+        """
+        originalName = "David"
+        studentOne = StudentClass.Student(originalName, "777777777", "Nate@aol.com", "134 Home St", 4.0, 150)
+        self.assertEquals(studentOne.name, originalName)
+
+
+    def test_ReturnRedId(self):
+        """
+        This unit test is testing the return of the 'redId' variable. It passes when a student's 'redId' variable
+        is returned by means of its property tag and cross-referenced to the original 'redId'.
+        :return:
+        """
+        originalRedId = 777777777
+        studentOne = StudentClass.Student("Nathan", originalRedId, "Nate@aol.com", "134 Home St", 4.0, 150)
+        self.assertEquals(studentOne.redId, originalRedId)
+
+    def test_UpperboundGpa(self):
+        """
+        This unit test is testing the upper bound of the 'gpa' variable. It passes when the 'gpa' is set to a number
+        greater than 4.0, thus raising a 'AttributeError'.
+        :return:
+        """
+        with self.assertRaises(AttributeError):
+            StudentClass.Student("Nathan", "777777777", "Nate@aol.com", "134 Home St", 4.1, 150)
 
     def test_UpperBoundNumberOfUnitsTaken(self):
         """
@@ -57,14 +164,59 @@ class TestStudentClass(unittest.TestCase):
         with self.assertRaises(AttributeError):
             StudentClass.Student("Nathan", "777777777", "Nate@aol.com", "134 Home St", 4.0, 151)
 
-    def test_LowerBoundNumberOfUnitsTaken(self):
+    def test_WrongTypeAddress(self):
         """
-        This unit test is testing the lower bound of the 'numberOfUnitsTaken' variable. It passes when the
-        'numberOfUnitsTaken' is set to a number lower than 0, thus raising a 'AttributeError'.
+        This unit test is testing the 'TypeError' check of the 'address' variable. It passes when the 'address' is set to something
+        that doesn't carry both ints and letters.
+        :return:
+        """
+        with self.assertRaises(TypeError):
+            StudentClass.Student("Nathan", "777777777", "Nate@aol.com", "123", "4.5", 150)
+
+    def test_WrongTypeGpa(self):
+        """
+        This unit test is testing the 'TypeError' check of the 'gpa' variable. It passes when the 'gpa' is set to something
+        other than an int or a float which triggers a 'TypeError'.
+        :return:
+        """
+        with self.assertRaises(TypeError):
+            StudentClass.Student("Nathan", "777777777", "Nate@aol.com", "134 Home St", "4.5", 150)
+
+    def test_WrongTypeEmail(self):
+        """
+        This unit test is testing the 'TypeError' check of the 'email' variable. It passes when something other than a
+        string is passed 'email'.
         :return:
         """
         with self.assertRaises(AttributeError):
-            StudentClass.Student("Nathan", "777777777", "Nate@aol.com", "134 Home St", 4.0, -0.1)
+            StudentClass.Student("Nathan", "777777777", 7, "134 Home St", "4.5", 150)
+
+    def test_WrongTypeName(self):
+        """
+        This unit test is testing the 'TypeError' check of the 'name' variable. It passes when a character other than the
+        letters A through Z are passed into 'name'.
+        :return:
+        """
+        with self.assertRaises(AttributeError):
+            StudentClass.Student(1, "777777777", "Nate@aol.com", "134 Home St", "4.5", 150)
+
+    def test_WrongTypeNumberOfUnitsTaken(self):
+        """
+        This unit test is testing the 'TypeError' check of the 'numberOfUnitsTaken' variable. It passes when the
+        'numberOfUnitsTaken' is set to something other than an int or a float which triggers a 'TypeError'.
+        :return:
+        """
+        with self.assertRaises(TypeError):
+            StudentClass.Student("Nathan", "777777777", "Nate@aol.com", "134 Home St", 4.0, "150")
+
+    def test_WrongTypeRedId(self):
+        """
+        This unit test is testing the 'redId' check of the 'redId' variable. It passes when a character other than a
+        number is passed into 'redId'.
+        :return:
+        """
+        with self.assertRaises(AttributeError):
+            StudentClass.Student(1, "AAAAAAAAA", "Nate@aol.com", "134 Home St", 4.5, 150)
 
 if __name__ == '__main__':
     unittest.main()
