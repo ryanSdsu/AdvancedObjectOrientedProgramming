0b5879063372bfd7ca30f9d3bb5fab01cc673d69
Ryan Ragasa <RJ@ryans-imac.home>
2018-09-14 21:16:45 -0700

    I created tests in the test_Node.py, specifically for the data and the init.

--- Assignment_Two/Assignment_Two_Unit_Tests/test_Node.py
+++ Assignment_Two/Assignment_Two_Unit_Tests/test_Node.py
@@ -1,11 +1,11 @@
 import unittest
 from Assignment_Two import NodeClass
 
-class TestStudentClass(unittest.TestCase):
+class TestNodeClass(unittest.TestCase):
 
-    def test_create_node(self):
+    def test_init_def(self):
         """
-        This unit test is testing the creation of a 'Node' object. It passes when a Node of various types are created
+        This unit test is testing the init of a 'Node' object. It passes when a Node of various types are created
         and compared to Nodes of similar types from the same class script of which they came.
         :return:
         """
@@ -19,6 +19,20 @@ class TestStudentClass(unittest.TestCase):
         self.assertIsInstance(NodeClass.Node(), type(list_node))
         multiple_argument_node = NodeClass.Node(1,2,3)
         self.assertIsInstance(NodeClass.Node(), type(multiple_argument_node))
+
+    def test_data_variable(self):
+        """
+        This unit test is testing the 'data' variable of the 'Node' class. It passes when upon proceeding with the
+        __init__() def that 'data' is being set to its arguments (it will be in a list form if there is more
+        than 1 argument) and it will be set to "None" if no arguments are being passed.
+        :return:
+        """
+        none_node = NodeClass.Node()
+        self.assertEquals(none_node.data, None)
+        one_argument_node = NodeClass.Node(1)
+        self.assertEquals(one_argument_node.data, 1)
+        multiple_argument_node = NodeClass.Node("Hi!", "I'm", "a", "Node")
+        self.assertEquals(multiple_argument_node.data, ["Hi!", "I'm", "a", "Node"])
 
 if __name__ == '__main__':
     unittest.main()
